de90717200c0c4031810ef4072fcb27af2a845e9
Belen Romero <anab.romero@bueh.edu.ar>
2026-05-26 10:14:09 -0300

    agrega comentarios descriptivos al ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,6 +4,7 @@ import { getByCategory, getProducts } from "../../services/productsService";
 import { useParams } from "react-router-dom";
 
 export const ItemListContainer = () => {
+  //tomamos "category" del useParams para poder filtrar
   const { category } = useParams();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -12,11 +13,13 @@ export const ItemListContainer = () => {
   useEffect(() => {
     setLoading(true);
 
+    //Usanmos la funcion con 2 uso: trae todo o filtra si category existe
     getByCategory(category)
       .then((data) => setProducts(data))
       .catch((err) => console.log("Hubo un error:", err))
       .finally(() => setLoading(false));
   }, [category]);
+  //👉No olvidar agregar "category" como variable de dependencias para que renderice nuevamente el container si cambia category
 
   if (loading) return <p>Cargando...</p>;
 
